4b6eeb7435ee9b0162180fdea699901818a46dd7
rnorback <rnorback@gmail.com>
2016-10-15 16:45:57 -0700

    Make position report report the actual position

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #include "BuildingEscape.h"
 #include "PositionReport.h"
@@ -18,8 +18,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty for the Chair"));
+    FString ObjectName = GetOwner()->GetName();
+    FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+    
+	UE_LOG(LogTemp, Warning, TEXT("%s is at location %s"), *ObjectName, *ObjectPos);
 }
 
 

667420edff8ce7755e1322304acc8c6e7b106438
rnorback <rnorback@gmail.com>
2016-10-15 21:29:46 -0700

    Create room

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,44 @@
+// Copyright Rob Norback 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    //Find the owning actor
+    AActor* Owner = GetOwner();
+    
+    //Create a rotator
+    FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+    
+    //Set the door rotation
+    Owner->SetActorRotation(NewRotation);
+    
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
